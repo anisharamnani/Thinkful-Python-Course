e3f458b713c1c48e53697cbd36dbaa8115e16f28
Anisha Ramnani <anisha.ramnani@gmail.com>
2013-08-29 19:23:49 -0400

    first commit

---
+++ LPTHW/ex5.py
@@ -0,0 +1,17 @@
+name = 'Anisha K. Ramnani'
+age = 21 # not a lie
+height = 65 # inches
+weight = 114 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name 
+print "She's %d inches tall." % height
+print "She's %d pounds heavy" % weight 
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair" % (eyes, hair)
+print "Her teeth are usually %s depending on the coffee" % teeth
+
+#this line is tricky, try to get it exactly right. 
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight) 
