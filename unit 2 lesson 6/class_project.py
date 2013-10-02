9e9559ff6bb826ec701fda5cd24ab4d64a0b71ee
Anisha Ramnani <anisha.ramnani@gmail.com>
2013-10-02 17:28:18 -0400

    two basic classes: Fish and Flounder

--- unit 2 lesson 6/class_project.py
+++ unit 2 lesson 6/class_project.py
@@ -1,1 +1,9 @@
-class Fish: 
+class Fish:
+	breathes_in_water = True
+	skin = "scales"
+	color: "green"
+	def move(self,speed): 
+			print "Swimming upright %s" % speed
+class Flounder(Fish): 
+	color: "blue"
+spencer = Fish()
